0d387ceb716531f266f9ef20e783382f8582e591
Tom Aldcroft <taldcroft@gmail.com>
2015-11-22 10:20:53 -0500

    Update for pkgutils namespace packages; update version to 0.7.1

---
+++ Ska/TelemArchive/__init__.py
@@ -0,0 +1,4 @@
+print('WARNING - Ska.Telemarchive module is deprecated and uses an '
+      'unmaintained data store.')
+
+__version__ = '0.7.1'
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
+
+from Ska.TelemArchive import __version__
+
 setup(name='Ska.TelemArchive',
       author = 'Tom Aldcroft',
-      description='Modules supporting SKA telemetry archive',
+      description='Modules supporting defunct SKA telemetry archive',
       author_email = 'aldcroft@head.cfa.harvard.edu',
       py_modules = ['Ska.TelemArchive.fetch',
                     'Ska.TelemArchive.fetch_client',
                     'Ska.TelemArchive.fetch_server',
                     'Ska.TelemArchive.data_table'],
-      version='0.07',
+      version=__version__,
       zip_safe=False,
-      namespace_packages=['Ska'],
       packages=['Ska', 'Ska.TelemArchive'],
       package_dir={'Ska' : 'Ska'},
       )
